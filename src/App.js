ff92a75a86b8235ee3b1c9c8ac8c30bc69516227
Greg <g.rutherford10@gmail.com>
2021-03-05 12:09:53 +0000

    changed the app to a class component

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
+import React, { Component } from "react";
 import "./App.css";
 
-function App() {
-	return (
-		<div className="App">
-			<header className="App-header">
-				<h2>IP Address Tracker</h2>
-				<input
-					type="text"
-					placeholder="Search for any IP Address or Domain"
-				></input>
-			</header>
-		</div>
-	);
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">
+					<h2>IP Address Tracker</h2>
+					<input
+						type="text"
+						placeholder="Search for any IP Address or Domain"
+					></input>
+				</header>
+			</div>
+		);
+	}
 }
 
 export default App;
